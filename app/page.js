3d2474e1240f2339d7735b66130cba87cac98a10
Abhishek Kamati <kamtiabhi4@gmail.com>
2024-07-31 12:04:52 +0530

    create landing page

--- app/page.js
+++ app/page.js
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Header from "./__components/Header";
+import Hero from "./__components/Hero";
 
 export default function Home() {
   return (
-    <div>Hello</div>
+    <div>
+      <Header/>
+      <Hero/> 
+    </div>
   );
 }
