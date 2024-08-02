6227e7be0cece820beb9d05350e8687c2ba94493
Abhishek Kamati <kamtiabhi4@gmail.com>
2024-08-02 15:49:16 +0530

    update keys

--- drizzle.config.js
+++ drizzle.config.js
@@ -2,7 +2,6 @@ export default {
     schema: "./utils/schema.jsx",
     dialect: 'postgresql',
     dbCredentials: {
-      url: 'postgresql://expenseDB_owner:POaNAiyF5U7n@ep-damp-wind-a5v2owpx.us-east-2.aws.neon.tech/expenseDB?sslmode=require',
-      // url: process.env.NEXT_PUBLIC_DATABASE_URL,
+      url: process.env.NEXT_PUBLIC_DATABASE_URL,
     }
   };
